bc28f754c8e2e78e1cc06a568a9672a22fd57b95
EvoxCz <evoxcz5@gmail.com>
2025-07-19 23:40:37 +0200

    Oprava textů v tlačítkách

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Funkce pro tlačítka
 
 function handleAppointment() {
-    alert('Objednání k lékaři\n\nTelefon: +420 123 456 789\nEmail: objednavky@meditz.cz\n\nOrdinační hodiny:\nPo-Pá: 7:00-18:00\nSo: 8:00-12:00');
+    alert('Objednání\n\nTelefon: +420 123 456 789\nEmail: objednavky@meditz.cz\n\nOtevírací doba:\nPo-Pá: 7:00-18:00\nSo: 8:00-12:00');
 }
 
 function handleEmergency() {
@@ -11,13 +11,13 @@ function handleEmergency() {
 }
 
 function handleContact() {
-    alert('Kontakt\n\nAdresa:\nZdravotnická 123\n110 00 Praha 1\n\nTelefon: +420 123 456 789\nEmail: info@meditz.cz\n\nOrdinační hodiny:\nPo-Pá: 7:00-18:00\nSo: 8:00-12:00');
+    alert('Kontakt\n\nAdresa:\nMeditz centrum\n110 00 Praha 1\n\nTelefon: +420 123 456 789\nEmail: info@meditz.cz\n\nOtevírací doba:\nPo-Pá: 7:00-18:00\nSo: 8:00-12:00');
 }
 
 function handleAbout() {
-    alert('O nás\n\nMeditz je moderní zdravotnické centrum s dlouholetou tradicí.\n\nNaším cílem je poskytovat kvalitní zdravotní péči s využitím nejnovějších technologií.\n\nNáš tým tvoří zkušení lékaři a zdravotní sestry.');
+    alert('O nás\n\nMeditz je moderní centrum s dlouholetou tradicí.\n\nNaším cílem je poskytovat kvalitní služby s využitím nejnovějších technologií.\n\nNáš tým tvoří zkušení odborníci.');
 }
 
 function handleServices() {
-    alert('Naše služby\n\n• Všeobecné lékařství\n• Interní medicína\n• Kardiologie\n• Dermatologie\n• Laboratorní vyšetření\n• EKG, ECHO\n• Ultrazvuk\n• Rentgen');
+    alert('Naše služby\n\n• Konzultace\n• Poradenství\n• Diagnostika\n• Vyšetření\n• Laboratorní služby\n• Preventivní péče\n• Odborné služby');
 }
